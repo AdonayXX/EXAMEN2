63514b8a861605c00b6da0f0513639d90956d3ed
Emerson Adonay Díaz Duarte <emdiazdu@est.utn.ac.cr>
2023-08-21 14:57:05 -0600

    Métodos delete, insert and search
    Se crean los métodos delete, search and insert en la clase ServicioList

--- src/Ticket.java
+++ src/Ticket.java
@@ -8,5 +8,32 @@
  * @author wendy
  */
 public class Ticket {
+    private int numero; 
+    private String Tipo; 
+
+    public Ticket() {
+    }
+
+    public Ticket(int numero, String Tipo) {
+        this.numero = numero;
+        this.Tipo = Tipo;
+    }
+    
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
     
 }
--- src/TicketList.java
+++ src/TicketList.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,6 +11,20 @@
  *
  * @author Emerson
  */
-public class TicketController {
-    
+public class TicketList {
+ private static TicketList ticketList;
+ private List<Ticket> ticket;
+
+ public TicketList() {
+ ticket= new ArrayList<>();   
+ }
+
 }
+
+
+
+
+
+
+
+
